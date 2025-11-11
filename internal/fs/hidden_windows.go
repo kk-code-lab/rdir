447bb5c6352a6c8143c9b4e49e2467e9389c46c0
kk-code-lab <github@xsxs.pl>
2025-11-11 16:54:24 +0100

    Hide Windows compatibility junctions

--- internal/fs/hidden_windows.go
+++ internal/fs/hidden_windows.go
@@ -2,41 +2,16 @@
 
 package fs
 
-import (
-	"os"
-	"syscall"
-)
-
 // IsHidden checks if a file is hidden on this platform (Windows)
 func IsHidden(fullPath string, name string) bool {
-	target := fullPath
-	if target == "" {
-		target = name
-	}
-	if target == "" {
+	if fullPath == "" && name == "" {
 		return false
 	}
 
-	ptr, err := syscall.UTF16PtrFromString(target)
+	attrs, err := getFileAttributes(fullPath, name)
 	if err != nil {
 		return len(name) > 0 && name[0] == '.'
 	}
 
-	attrs, err := syscall.GetFileAttributes(ptr)
-	if err != nil {
-		if os.IsNotExist(err) && fullPath != name && fullPath != "" {
-			ptrAlt, convErr := syscall.UTF16PtrFromString(name)
-			if convErr == nil {
-				if attrsAlt, errAlt := syscall.GetFileAttributes(ptrAlt); errAlt == nil {
-					attrs = attrsAlt
-					err = nil
-				}
-			}
-		}
-		if err != nil {
-			return len(name) > 0 && name[0] == '.'
-		}
-	}
-	const FILE_ATTRIBUTE_HIDDEN = 0x02
-	return attrs&FILE_ATTRIBUTE_HIDDEN != 0
+	return attrs&fileAttributeHidden != 0
 }
